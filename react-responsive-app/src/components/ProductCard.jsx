1f7ca40e7d1995735d7ad627c77506922fb19389
Kelvin <kelvinlieputra07@gmail.com>
2026-04-18 13:35:33 +0700

    tambah r jadi 2

--- react-responsive-app/src/components/ProductCard.jsx
+++ react-responsive-app/src/components/ProductCard.jsx
@@ -8,7 +8,7 @@ const ProductCard = ({ product }) => {
       {product.isBestseller === 1 && (
         <div className="absolute top-4 right-4 z-10">
           <span className="bg-red-500 text-white text-xs font-bold px-3 py-1 rounded-full shadow-lg">
-            Best Seller 🔥
+            Best Sellerr 🔥
           </span>
         </div>
       )}
